8b20ca9d5fd5f768b6eb67808829cc4d1bb1e388
David Merwin <dave@davemerwin.com>
2008-09-07 00:03:03 -0700

    tweak UI and basic functionality

--- media/js/djDebug.settings.js
+++ media/js/djDebug.settings.js
@@ -1,14 +1,21 @@
 $(document).ready(function(){
     $('#djDebugToolbar').hide();
+    $('.panelContentView').hide();
     
     // Style Settings
     $('#djDebugBlock').css({position: 'relative', background: 'orange', fontFamily: 'Helvetica, Arial, sans-serif'});
     
     $('#debugButton').css({background: 'orange', position: 'absolute', bottom: '-1', right: '10px', width: '60px', height: '20px', textAlign: 'center', fontSize: '12px', lineHeight: '20px', textDecoration: 'none', color: 'red', fontWeight: 'bold', outline: 'none'});
     
-    $('.panelContent').css({padding: '20px'});
+    $('#djDebugToolbar').css({position: 'absolute', width: '300px', top: '0px', right: '0px', overflow: 'auto', background: 'orange'});
     
-    $('#djDebugPanelList').css({margin: '0'})
+    $('.panelContent').css({padding: '20px'});
+    $('.panelToggle').click(function() {
+        $(this).parent().children('.panelContentView').slideToggle();
+        return false
+    });
+    
+    $('#djDebugPanelList').css({margin: '20px 0px'})
     
     //Actions
     $('#debugButton').click(function() {
